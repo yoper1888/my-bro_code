13d6fb4b44bdbf9554ee0827e2f6e97b6576a5ee
yoper1888 <yoperdaniel46@gmail.com>
2024-04-18 01:09:13 +0300

    firstly uplode

--- src/Student.jsx
+++ src/Student.jsx
@@ -1,17 +1,17 @@
 
-function Student(props) {
-    return (
-        <div>
-            <p>Name:{props.name}</p>
-            <p>Age:{props.age}</p>
-            <p>Student:{props.isStudents ? "yes" : "No"}</p>
+// function Student(props) {
+//     return (
+//         <div>
+//             <p>Name:{props.name}</p>
+//             <p>Age:{props.age}</p>
+//             <p>Student:{props.isStudents ? "yes" : "No"}</p>
 
-        </div>
-    )
-}
-Student.defaultprops = {
-    name: "guest",
-    age: 0,
-    isStudents: no,
-}
-export default Student;
+//         </div>
+//     )
+// }
+// Student.defaultprops = {
+//     name: "guest",
+//     age: 0,
+//     isStudents: no,
+// }
+// export default Student;
